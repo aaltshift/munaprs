266e867b87c8c7d54027ebbdc65a1019d3079200
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-04-13 10:49:45 -0400

    Updated

--- source/nothinbutdotnetstore/CommandRegistry.cs
+++ source/nothinbutdotnetstore/CommandRegistry.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace nothinbutdotnetstore
 {
     public class CommandRegistry : ICanFindCommandsThatCanProcessRequests
     {
-        
+        public ICanProcessOneUniqueRequest get_the_command_that_can_handle(IContainRequestDetails request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
