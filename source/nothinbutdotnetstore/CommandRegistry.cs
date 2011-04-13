d6d4cf7767c5582dbe2ff6565be24677a458a7fb
pgarbe <philipp.garbe@web.de>
2011-04-13 16:55:56 +0200

    Committing

--- source/nothinbutdotnetstore/CommandRegistry.cs
+++ source/nothinbutdotnetstore/CommandRegistry.cs
@@ -1,12 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore
 {
     public class CommandRegistry : ICanFindCommandsThatCanProcessRequests
     {
+        private readonly IEnumerable<ICanProcessOneUniqueRequest> allCommands;
+
+        public CommandRegistry(IEnumerable<ICanProcessOneUniqueRequest> allCommands)
+        {
+            this.allCommands = allCommands;
+        }
+
         public ICanProcessOneUniqueRequest get_the_command_that_can_handle(IContainRequestDetails request)
         {
-            throw new NotImplementedException();
+            foreach (var canProcessOneUniqueRequest in allCommands)
+            {
+                if (canProcessOneUniqueRequest.can_process(request))
+                    return canProcessOneUniqueRequest;
+            }
+
+            return null;
         }
     }
 }
