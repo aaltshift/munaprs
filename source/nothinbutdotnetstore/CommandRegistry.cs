33976fb35c2396874c05da5460131ab51d517743
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-04-13 11:30:36 -0400

    Updated

--- source/nothinbutdotnetstore/CommandRegistry.cs
+++ source/nothinbutdotnetstore/CommandRegistry.cs
@@ -1,21 +1,23 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace nothinbutdotnetstore {
-    public class CommandRegistry : ICanFindCommandsThatCanProcessRequests {
-        readonly IEnumerable<ICanProcessOneUniqueRequest> all_commands;
-        private MissingCommandFactory missingCommandFactory;
+namespace nothinbutdotnetstore
+{
+    public class CommandRegistry : ICanFindCommandsThatCanProcessRequests
+    {
+        IEnumerable<ICanProcessOneUniqueRequest> all_commands;
+        MissingCommandFactory missing_command_factory;
 
-        public CommandRegistry(IEnumerable<ICanProcessOneUniqueRequest> allCommands, MissingCommandFactory missingCommandFactory) {
-            this.all_commands = allCommands;
-            this.missingCommandFactory = missingCommandFactory;
+        public CommandRegistry(IEnumerable<ICanProcessOneUniqueRequest> all_commands,
+                               MissingCommandFactory missingCommandFactory)
+        {
+            this.all_commands = all_commands;
+            this.missing_command_factory = missingCommandFactory;
         }
 
-        public ICanProcessOneUniqueRequest get_the_command_that_can_handle(IContainRequestDetails request) {
-
-            ICanProcessOneUniqueRequest command = all_commands.FirstOrDefault(x => x.can_process(request));
-            return command ?? missingCommandFactory();
+        public ICanProcessOneUniqueRequest get_the_command_that_can_handle(IContainRequestDetails request)
+        {
+            return all_commands.FirstOrDefault(x => x.can_process(request)) ?? missing_command_factory();
         }
-
     }
 }
