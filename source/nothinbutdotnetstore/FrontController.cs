67892e7fe356f2e643331c9cdccae85863ff1ec8
Alisa Altshuler <aaltshuler@autoscout24.com>
2011-04-13 16:28:03 +0200

    Pushing new changes

--- source/nothinbutdotnetstore/FrontController.cs
+++ source/nothinbutdotnetstore/FrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore
 {
     public class FrontController : IProcessIncomingWebRequests
     {
+        private readonly ICanFindCommandsThatCanProcessRequests command_registry;
+
+        public FrontController(ICanFindCommandsThatCanProcessRequests command_registry)
+        {
+            this.command_registry = command_registry;
+        }
+
         public void process(IContainRequestDetails the_request)
         {
-            throw new NotImplementedException();
+            var command = this.command_registry.get_the_command_that_can_handle(the_request);
+            command.run_using(the_request);
         }
     }
 }
