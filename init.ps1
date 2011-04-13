079d4bec056239e2be6284e9202503cdb826ffa5
jenrom <jenrom@gmail.com>
2011-04-13 14:34:57 +0200

    Committing

--- init.ps1
+++ init.ps1
@@ -1,7 +1,4 @@
-"Copying configuration files"
-cp build\local_properties.rb.customize build\local_properties.rb
-cp build\tasks\configuration_db.rb.customize build\tasks\configuration_db.rb
 
 "Creating the remote link to developwithpassion code base"
 git remote rm jp
-git remote add jp git://github.com/developwithpassion/munaprp.git
+git remote add jp http://github.com/developwithpassion/munaprs.git
