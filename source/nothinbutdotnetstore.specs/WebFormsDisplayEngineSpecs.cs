7cdd09d3290daa65fb9767551c305df68ab41d2d
Alisa Altshuler <aaltshuler@autoscout24.com>
2011-04-15 12:23:35 +0200

    Pushing new changes

--- source/nothinbutdotnetstore.specs/WebFormsDisplayEngineSpecs.cs
+++ source/nothinbutdotnetstore.specs/WebFormsDisplayEngineSpecs.cs
@@ -6,6 +6,7 @@
 namespace nothinbutdotnetstore.specs
 {
     using System;
+    using System.Web;
 
     using nothinbutdotnetstore.web.core;
 
@@ -21,31 +22,43 @@ namespace nothinbutdotnetstore.specs
         public class when_display : concern
         {
             Establish c = () =>
-                { 
+                {
+                    view_registry = depends.on<ICanFindViewsThatCanDisplayReportModels>();
+
                     the_report_model = fake.an<object>();
 
+                    view_registry.setup(x => x.get_the_view_name_that_can_display(the_report_model)).Return("view");
                 };
 
             Because b = () => sut.display(the_report_model);
 
             private It should_call_the_getter_on_the_view_registry =
-                () => { view_registry.get_the_view_that_can_display(the_report_model); };
+                () => { view_registry.get_the_view_name_that_can_display(the_report_model); };
 
             static object the_report_model;
             static ICanFindViewsThatCanDisplayReportModels view_registry;
+            private static HttpContext http_context;
         }
     }
 
     public class WebFormsDisplayEngine : ICanDisplayReportModels
     {
+        ICanFindViewsThatCanDisplayReportModels view_registry;
+
+        public WebFormsDisplayEngine(ICanFindViewsThatCanDisplayReportModels view_registry)
+        {
+            this.view_registry = view_registry;
+        }
+
         public void display<ReportModel>(ReportModel report_model)
         {
-            throw new NotImplementedException();
+            string view_name = view_registry.get_the_view_name_that_can_display(report_model);
+            // ...
         }
     }
 
     public interface ICanFindViewsThatCanDisplayReportModels
     {
-        object get_the_view_that_can_display(object theReportModel);
+        string get_the_view_name_that_can_display(object theReportModel);
     }
 }
